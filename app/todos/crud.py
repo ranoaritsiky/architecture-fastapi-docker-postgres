4b21528a4a16ecff18e8d8d943f5987661f8bdd8
Tsiky <tsiky@test.com>
2022-11-22 16:50:55 +0300

    add create and read data

--- app/todos/crud.py
+++ app/todos/crud.py
@@ -3,5 +3,23 @@ from ..models import models
 from ..schemas import schemas
 
 
-def get_todo(db:Session, todo_id:int):
+def get_todos(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Todos).offset(skip).limit(limit).all()
+
+def get_todo(db: Session, todo_id: int):
     return db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+def get_todo_by_name(db: Session, name: str):
+    return db.query(models.Todos).filter(models.Todos.name == name).first()
+
+def create_todo(db: Session, todo: schemas.TodoCreate):
+    db_todo = models.Todos(name=todo.name, description=todo.description)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+def delete_todo(db: Session, todo: schemas.TodoCreate):
+    db_todo = db.query(models.Todos).filter(models.Todos.name == todo.name).first()
+    db.delete(db_todo)
+    db.commit()
