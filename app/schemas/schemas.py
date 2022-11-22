99657c5b8df20c60e3dc915cd86016a38e779554
Tsiky <tsiky@test.com>
2022-11-22 16:50:33 +0300

    update todo schemas

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -5,8 +5,11 @@ from pydantic import BaseModel
 class TodosBase(BaseModel):
     name:str
     
-class Todo(TodosBase):
+class TodoCreate(TodosBase):
     description:str
     
+class Todo(TodosBase):
+    pass    
+
     class Config:
         orm_mode = True
